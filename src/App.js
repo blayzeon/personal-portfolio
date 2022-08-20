9459aa0b0b09a318511fda339d0e61258a78f4b9
blayzeon <blayzeon@gmail.com>
2022-08-20 08:38:43 -0400

    dynamic header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ function App() {
   return (
     <div className="App">
       <header>
-        <div className="curved1"></div>
+        <div className="curved"></div>
+        <div className="shadow">
+          <div className="margin flex">
+            <div className="img">
+              <h1 className="subtext">blayzeon</h1>
+            </div>
+            <div className="blurb">
+              <h2>About Me</h2>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+                libero sint asperiores saepe nemo minima! Eum aperiam possimus
+                earum inventore quasi dolorum magnam itaque ducimus, quod odio
+                debitis placeat perspiciatis!
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
 
       <main className="margin">
